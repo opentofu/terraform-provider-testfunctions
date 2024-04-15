3b71388d332f39f9806eb8780a760abe12739076
James Humphries <james@james-humphries.co.uk>
2024-04-15 15:31:25 +0100

    removed testAccPreCheck
    Signed-off-by: James Humphries <james@james-humphries.co.uk>

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -6,8 +6,6 @@
 package provider
 
 import (
-	"testing"
-
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
@@ -18,8 +16,4 @@ import (
 // reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"testfunctions": providerserver.NewProtocol6WithError(New("test")()),
-}
-
-func testAccPreCheck(t *testing.T) {
-
 }
